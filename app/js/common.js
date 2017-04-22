0e5755f2d51bca129b7ba73527b4d343ddf7fdf5
Taras <kalini.taras@gmail.com>
2017-04-22 16:38:02 +0300

    taraskalini

---
+++ app/js/common.js
@@ -0,0 +1,124 @@
+$(function() {
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+    $('.popup').magnificPopup();
+
+    $('.gamburger').on('click',function(){
+        $(this).toggleClass('active');
+        $(".header__top__mnu ul").toggleClass('active');
+    });
+    $(document).on("click",function(event){
+        if( $(event.target).closest(".header__top__mnu ul,.gamburger").length )return;
+        $('.gamburger').removeClass('active');
+        $(".header__top__mnu ul").removeClass('active');
+        event.stopPropagation();
+    });
+
+    $("a[rel='m_PageScroll2id']").mPageScroll2id();
+
+    $("a[rel='m_PageScroll2id']").on("click",function(){
+        $('.gamburger').removeClass('active');
+        $(".header__top__mnu ul").removeClass('active');
+    });
+
+    $('.btn-close').on("click",function(event){
+        $.magnificPopup.close();
+    });
+
+
+    $('.gallery__content__item').magnificPopup({
+        type: 'image',
+        gallery:{
+            enabled:true
+        },
+        mainClass: "gallery-close"
+    });
+
+});
+
+//Форма отправки 2.0
+$(function() {
+    $("[name=send]").click(function () {
+        $(":input.error").removeClass('error');
+        $(".allert").remove();
+
+        var error;
+        var btn = $(this);
+        var ref = btn.closest('form').find('[required]');
+        var msg = btn.closest('form').find('input, textarea');
+        var send_btn = btn.closest('form').find('[name=send]');
+        var subject = btn.closest('form').find('[name=form_subject]');
+        $(ref).each(function () {
+            if ($(this).val() == '') {
+                var errorfield = $(this);
+                $(this).addClass('error').parent('.field').append('<div class="allert"><span>Заполните это поле</span><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></div>');
+                error = 1;
+                $(":input.error:first").focus();
+                return;
+            } else {
+                var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+                if ($(this).attr("type") == 'email') {
+                    if (!pattern.test($(this).val())) {
+                        $("[name=email]").val('');
+                        $(this).addClass('error').parent('.field').append('<div class="allert"><span>Укажите коректный e-mail</span><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></div>');
+                        error = 1;
+                        $(":input.error:first").focus();
+                    }
+                }
+                var patterntel = /^()[0-9]{9,18}/i;
+                if ($(this).attr("type") == 'tel') {
+                    if (!patterntel.test($(this).val())) {
+                        $("[name=phone]").val('');
+                        $(this).addClass('error').parent('.field').append('<div class="allert"><span>Укажите коректный номер телефона</span><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></div>');
+                        error = 1;
+                        $(":input.error:first").focus();
+                    }
+                }
+            }
+        });
+        if (!(error == 1)) {
+            $(send_btn).each(function () {
+                $(this).attr('disabled', true);
+            });
+
+            var form = btn.closest('form'), name = form.find('[name=name]').val();
+
+            $.ajax({
+                type: 'POST',
+                url: 'mail.php',
+                data: msg,
+                success: function (data) {
+                    $.magnificPopup.close();
+                    form[0].reset();
+                    $(send_btn).each(function () {
+                        $(this).attr('disabled', false);
+                    });
+
+                    if(subject == "Заказать звонок"){
+                        $("a[href='#popupthx']").click();
+                    }else{
+                        $("a[href='#block-popup']").click();
+                    }
+
+
+                },
+                error: function (xhr, str) {
+                    alert('Возникла ошибка: ' + xhr.responseCode);
+                }
+            });
+        }
+        return false;
+    });
+});
+
